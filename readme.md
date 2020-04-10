d871a52011a99557b079d129a83f14edba6b8e48
taoqf <tao_qiufeng@126.com>
2020-04-10 16:18:21 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
 # 日期选择
+
+## 属性
+
+参见[链接](https://github.com/mmazzarolo/react-native-modal-datetime-picker#available-props)
+
+主要涉及的几个属性
+
+### mode
+
+- `datetime`
+
+	同时选日期和时间,android上分为两步
+
+- `date`
+
+	选择日期
+
+- `time`
+
+	选择时间
+
+### onConfirm
+
+确认事件
+
+### isVisible
+
+是否显示，设置为true时显示，默认不显示
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DateTimePicker, { DateTimePickerProps } from 'react-native-modal-datetime-picker';
 
-interface IProps {
+export interface IProps extends Omit<DateTimePickerProps, 'onCancel'> {
+	minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30 | undefined;
+	onCancel?(): void;
 }
 
-export default function Widget(props: IProps) {
-	return <>
-	</>;
+export default function TimePicker(p: IProps) {
+	const { onCancel, ...props } = p;
+	// 标记一下，解决mode为datetime时选择完日期后无法再重新选择日期问题
+	const [reset, setreset] = useState(false);
+	useEffect(() => {
+		if (p.mode === 'datetime') {
+			setreset(false);
+		}
+	}, [p]);
+	if (p.mode === 'datetime' && reset) {
+		// 这里作用是当选择完之后重新构建控件，这样如果mode为datetime时，将重新回到选择日期界面
+		return <></>;
+	}
+	return (
+		<DateTimePicker
+			{...props}
+			onCancel={() => {
+				if (onCancel) {
+					onCancel();
+				}
+				if (p.mode === 'datetime') {
+					// 这里作用是当选择完之后重新构建控件，这样如果mode为datetime时，将重新回到选择日期界面
+					setreset(true);
+				}
+			}}
+			onConfirm={(date: Date) => {
+				props.onConfirm(date);
+				if (p.mode === 'datetime') {
+					// 这里作用是当选择完之后重新构建控件，这样如果mode为datetime时，将重新回到选择日期界面
+					setreset(true);
+				}
+			}}
+			onDateChange={(newDate: Date) => {
+				if (props.onDateChange) {
+					props.onDateChange(newDate);
+				}
+			}}
+		/>
+	);
 }
